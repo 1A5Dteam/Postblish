91e06d0b3c05b2afe153a678ebf5958488681798
daitran1412 <74389731+daitran1412@users.noreply.github.com>
2020-11-18 10:06:29 +0700

    Update Login.java

--- app/src/main/java/com/example/application/Login.java
+++ app/src/main/java/com/example/application/Login.java
@@ -43,7 +43,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        /* My comment :> */
         /*
         try {
             PackageInfo info = null;
